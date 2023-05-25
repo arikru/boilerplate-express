ead05fc4c328d8e6dbb00eb73f5eb54097cea4d7
arikru <krumbach.arian@gmail.com>
2023-05-25 22:48:22 +0200

    first commit

--- myApp.js
+++ myApp.js
@@ -2,7 +2,16 @@ let express = require('express');
 let app = express();
 
 
+// console.log("Hello World");
 
+let absolutePath = __dirname + '/views/index.html';
+let assetPath = __dirname + "/public";
+
+app.use("/public", express.static(assetPath));
+
+app.get("/", (req, res) => {
+  res.sendFile(absolutePath);
+})
 
 
 
