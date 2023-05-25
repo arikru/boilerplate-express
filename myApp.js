a99c2cb96f48aa543e1b641dc2421136849dd5aa
arikru <krumbach.arian@gmail.com>
2023-05-26 00:23:25 +0200

    Worked on assignments up until no. 6

--- myApp.js
+++ myApp.js
@@ -1,19 +1,34 @@
 let express = require('express');
 let app = express();
-
+require('dotenv').config()
 
 // console.log("Hello World");
-
 let absolutePath = __dirname + '/views/index.html';
 let assetPath = __dirname + "/public";
 
+let modString = "Hello json"
+let baseJson = {"message": modString};
+
 app.use("/public", express.static(assetPath));
+app.use(function middleware(req, res, next) {
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+});
 
 app.get("/", (req, res) => {
   res.sendFile(absolutePath);
 })
 
-
+app.get("/json", (req, res) => {
+  let messageStyle = process.env.MESSAGE_STYLE;
+  console.log(messageStyle);
+  if (messageStyle === "uppercase") {
+    res.json({"message": modString.toUpperCase()});
+  } else {
+    res.json(baseJson);
+  }
+  
+})
 
 
 
